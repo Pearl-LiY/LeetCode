a1b5cff5083ec8f8d2093ab21342a4c16104c721
Pearl-LiY <yuge.cam@outlook.com>
2026-03-23 21:03:30 +0800

    LC-lru-cache [Runtime: N/A; Memory: N/A]

---
+++ hash-table/lru-cache.py
@@ -0,0 +1,77 @@
+class Node(object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = {}
+        self.head = None
+        self.tail = None
+    
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.cache:
+            return -1
+        
+        node = self.cache[key]
+        self.move_to_head(node)
+        return node.val
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.move_to_head(node)
+            return
+        
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        self.add_node(new_node)
+
+        if len(self.cache) > self.capacity:
+            tail_node = self.tail
+            self.remove_node(tail_node)
+            del self.cache[tail_node.key]
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+    def add_node(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else: 
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+    def remove_node(self, node):
+        if node.prev:
+            node.prev.next = node.next
+        if node.next:
+            node.next.prev = node.prev
+        if node == self.head:
+            self.head = node.next
+        if node == self.tail:
+            self.tail = node.prev
+    def move_to_head(self, node):
+        self.remove_node(node)
+        self.add_node(node)
